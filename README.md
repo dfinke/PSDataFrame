b811957bf8ac9d0e2962b0d7012036559e1657d0
dfinke <finked@hotmail.com>
2019-12-31 10:51:26 -0500

    intial commit

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
+<!-- chapter start -->
+
 # PSDataFrame
+
+Microsoft announced the preview of a DataFrame type for .NET to make data exploration easy. https://devblogs.microsoft.com/dotnet/an-introduction-to-dataframe/
+
+The functions n `cvtToDF` is a proof of concept that wraps it in PowerShell so you can easily transform Powershell arrays into a DataFrame and then work with it.
+
+## Test data
+
+|Region|Item|TotalSold|DateSold|Factor|
+|---|---|---|---|---|
+West|drill|29|12/2|2.1
+South|lime|19|12/21|2.1
+West|nail|57|12/23|2.1
+West|melon|1|12/12|2.1
+North|saw|88|12/22|2.1
+South|avocado|42|12/24|2.1
+North|screws|86|12/25|2.1
+West|avocado|7|12/27|2.1
+East|avocado|83|12/29|2.1
+West|drill|89|12/28|2.1
+
+## PowerShell CSV and DataFrames
+
+```ps
+. .\cvtToDF.ps1
+
+(ConvertTo-DataFrame (Import-Csv .\testData.csv)).groupby("Region").Sum("TotalSold").Sort("Region") | Out-DataFrame
+```
+
+```
+Region TotalSold
+------ ---------
+East          83
+North        174
+South         61
+West         183
+```
+
+## PowerShell Excel and DataFrames
+
+```ps
+. .\cvtToDF.ps1
+
+(ConvertTo-DataFrame (Import-Excel .\testData.xlsx)).groupby("Region").Sum("TotalSold").Sort("Region") | Out-DataFrame
+```
+
+```
+Region TotalSold
+------ ---------
+East          83
+North        174
+South         61
+West         183
+```
+<!-- chapter end -->
---
+++ cvtToDF.ps1
@@ -0,0 +1,100 @@
+$basePath = ".\bin"
+
+$null = [System.Reflection.Assembly]::LoadFrom("$basePath\Apache.Arrow.dll")
+$null = [System.Reflection.Assembly]::LoadFrom("$basePath\Microsoft.ML.DataView.dll")
+$null = [System.Reflection.Assembly]::LoadFrom("$basePath\System.Runtime.CompilerServices.Unsafe.dll")
+$null = [System.Reflection.Assembly]::LoadFrom("$basePath\Microsoft.Data.Analysis.dll")
+
+. '.\InferData\InferData.ps1'
+
+function Get-DataTypeInfo {
+    param(
+        $InputObject
+    )
+
+    $propertyNames = $InputObject[0].psobject.Properties.name
+
+    foreach ($name in $propertyNames) {
+        $r = Invoke-TestSet $InputObject[0].$name -First -OnlyPass
+        [PSCustomObject][Ordered]@{
+            PropertyName = $name
+            DataType     = $r.DataType
+        }
+    }
+}
+
+function ConvertTo-DataFrame {
+    param(
+        $InputObject
+    )
+
+    $column = @()
+    $colCount = 0
+    $dataInfo = Get-DataTypeInfo $InputObject
+
+    $dataInfo | ForEach-Object {
+        if ($_.DataType -eq 'string') {
+            $str = 'New-Object Microsoft.Data.Analysis.StringDataFrameColumn "{0}", {1}' -f $_.PropertyName, $InputObject.Count
+        }
+        else {
+            $str = 'New-Object Microsoft.Data.Analysis.PrimitiveDataFrameColumn[{0}] "{1}", {2}' -f $_.DataType, $_.PropertyName, $InputObject.Count
+        }
+
+        $vName = "Col_$($colCount)"
+        $column += $vName
+        $v = ($str | Invoke-Expression)
+        Set-Variable -Name "$vName" -Value $v
+        $colCount++
+    }
+
+    $dfStr = 'New-Object Microsoft.Data.Analysis.DataFrame ${0}' -f ($Column -join ', $')
+    $df = $dfStr | Invoke-Expression
+
+    $row = 0
+    foreach ($item in $InputObject) {
+        foreach ($col in 0..($dataInfo.Count - 1)) {
+            $df[$row, $col] = $item.($dataInfo[$col].propertyname) -as $dataInfo[$col].datatype
+        }
+        $row++
+    }
+
+    $df
+}
+
+function Out-DataFrame {
+    param(
+        [Parameter(ValueFromPipeline)]
+        $dataFrame
+    )
+
+    Process {
+        $names = $dataFrame.Columns.Name
+        for ($i = 0; $i -lt $dataFrame.Rows.Count; $i++) {
+
+            $h = [ordered]@{ }
+            $colCount = 0
+            foreach ($column in $dataFrame.Rows[$i]) {
+                $colName = $names[$colCount++]
+                $h.$colName = $column
+            }
+
+            [PSCustomObject]$h
+        }
+    }
+}
+
+Set-Alias -Name odf -Value Out-DataFrame
+
+function Add-ToDF {
+    param(
+        $targetDF,
+        $ColumnName,
+        $value,
+        [Switch]$Inplace
+    )
+
+    $flag = $false
+    if ($Inplace) { $flag = $true }
+
+    $targetDF[$ColumnName].Add($value, $flag)
+}
